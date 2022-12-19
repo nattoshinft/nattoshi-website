0c81a13018435fa03979b9348d0af67cdc14bdca
Nattoshi <nattoshinft@gmail.com>
2022-12-19 19:34:29 +0900

    update build

--- src/index.js
+++ src/index.js
@@ -3,10 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ringer from "./Assets/My Chemical Romance - Helena.mp3";
+
+const Sound = () => {
+  const audio = new Audio(ringer);
+  audio.loop = true;
+
+  return (
+    <div className='soundButton'>
+      <button
+        onClick={() => {
+          audio.loop = true;
+          audio.play();
+        }}
+      >
+        Play
+      </button>
+      <button onClick={() => (audio.loop = false)}>Pause</button>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+        <Sound />
     <App />
   </React.StrictMode>
 );
@@ -15,3 +36,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
